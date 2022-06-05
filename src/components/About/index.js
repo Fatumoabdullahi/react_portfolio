e7b72f88f39e0e04680425f7d91701ae95e7c65f
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-06-05 17:11:45 +0100

    working application

---
+++ src/components/About/index.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm an ambitious full stack developer with a background in
+            linguistics and teaching, hoping to kick start my career in software
+            development. I am looking for a role in a well established IT
+            company with the opportunity to work with the latest technologies on
+            challenging and diverse projects.
+          </p>
+          <p align="LEFT">
+            I am a confident and ambitious learner, a keen individual with an
+            interest in tech. I'm a risk taker who is always up for a challenge.
+          </p>
+          <p>
+            If I was to define myself I would say that I am a friendly
+            individual who enjoys nature in all its aspects, a good book and the
+            odd coffee from time to time.
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,65 @@
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/fatumo-abdullahi-11a663199"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/fatumoabdullahi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCnK4_KBYM6jpx4Q7n9GvgTA"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
